206ea6e380db9cd9e71089bf9ec581a6e0c4ff25
Marco Grier <mlgrier@gmail.com>
2017-03-28 20:05:40 -0500

    Updated the code to update the labels

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -12,10 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
     @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
     var currentValue: Int = 0
-    
     var targetValue: Int = 0
+    var score: Int = 0
+    var round: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,8 +35,11 @@ class ViewController: UIViewController {
 
     @IBAction func showAlert() {
         let difference = abs(targetValue - currentValue)
+        let points = 100 - difference
         
-        let message = "The value of the slider is: \(currentValue)" + "\nThe target value is: \(targetValue)" + "\nThe difference is: \(difference)"
+        score += points
+        
+        let message = "You scored \(points) points"
         
         let alert = UIAlertController(title: "Hello, World", message: message, preferredStyle: .alert)
         
@@ -51,13 +57,18 @@ class ViewController: UIViewController {
     }
     
     func startNewRound() {
+        round += 1
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+        
     }
     
     func updateLabels() {
         targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
     }
+    
 }
 
